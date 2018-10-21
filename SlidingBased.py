2ec4b1ed12d123df3330d6e1c93cb3f6fc21060a
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-10-21 13:42:01 +0330

    Calculating Single Sliding Door and Windows

--- SlidingBased.py
+++ SlidingBased.py
@@ -1,41 +1,58 @@
-width = 2.40
-height = 1.13
-parts = 3
-debug = 1
-SashNum = 2
+width = input("Width: ")
+height = input("Height: ")
+parts = input("Parts of Window: ")
+SashNum = input("Number of Sashes: ")
 
+width = float(width)
+height = float(height)
+parts = int(parts)
+SashNum = int(SashNum)
+
+debug = 1
+# .....................................................
 # Sum all 4 sides of Frames
-FrameSum = ((width*2)+(height*2))*35000
-if(debug==1): print("Frame:",FrameSum )
+FrameSum = ((width*2)+(height*2))*85000
+if debug == 1:
+    print("Frame:", FrameSum)
 # Mullions
-Mullions = ((parts-1)*(height))*30000
-if(debug==1): print("Mullions:",Mullions )
+Mullions = ((parts-1)*(height))*75000
+if debug == 1:
+    print("Mullions:", Mullions)
 # Sash
-Sashes = SashNum*(((width/parts)*2)+(height*2))*30000
-if(debug==1): print("Sashes:",Sashes )
+Sashes = SashNum*(((width/parts)*2)+(height*2))*75000
+if debug == 1:
+    print("Sashes:", Sashes)
 # Interlock
-Interlock = SashNum*((height)*7000)
-if(debug==1): print("InterLock:",Interlock )
+Interlock = SashNum*((height)*15000)
+if debug == 1:
+    print("InterLock:", Interlock)
 # Overlapping Cover
-OCover = SashNum*((((width/parts)*2)+(height*2))*7000)
-if(debug==1): print("Overlapping Cover:",OCover)
+OCover = SashNum*((((width/parts)*2)+(height*2))*15000)
+if debug == 1:
+    print("Overlapping Cover:", OCover)
 # Alminuim Rail
-Arail = width * 5000
-if(debug==1): print("Alminuim Rail:",Arail )
+Arail = width * 10000
+if debug == 1:
+    print("Alminuim Rail:", Arail)
 # Tape
-Tape = (SashNum*(((width/parts)*2)+height))*700
-if(debug==1): print("Tape:",Tape )
+Tape = (SashNum*(((width/parts)*2)+height))*1200
+if debug == 1:
+    print("Tape:", Tape)
 # Equipment
-Equipment = SashNum*75000
-if(debug==1): print("Equipment:",Equipment )
+Equipment = SashNum*200000
+if debug == 1:
+    print("Equipment:", Equipment)
 # Glass
-Glass = ((width*height)*0.7)*50000
-if(debug==1): print("Glass:",Glass)
+Glass = ((width*height)*0.7)*77000
+if debug == 1:
+    print("Glass:", Glass)
 # Installation Equipment
-Iequip = SashNum*20000
-if(debug==1): print("Installation Material:",Iequip )
+Iequip = SashNum*40000
+if debug == 1:
+    print("Installation Material:", Iequip)
 
 FinalPrice = FrameSum+Mullions+Sashes+Interlock+OCover+Arail+Tape+Tape+Equipment+Glass+Iequip
 FinalPrice += (FinalPrice*10)/100
-if(debug==1): print("Installation Price: 10 Percent",(FinalPrice*10)/100)
-print("Total price of Sliding Door and window:",FinalPrice)
+if debug == 1:
+    print("Installation Price: 10 Percent", (FinalPrice*10)/100)
+print("Total price of Sliding Door and window:", FinalPrice)
